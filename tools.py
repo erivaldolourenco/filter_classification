49fcd090abd7b62b96de5b0edf86418cd5543e73
Erivaldo Lourenço <erivaldolourenco.m@gmail.com>
2020-04-04 00:19:14 -0300

    Arquivo de tools

--- tools.py
+++ tools.py
@@ -1,4 +1,7 @@
-
+'''
+Ler uma linha do arquivo de dados e retorna a linha já sem caracteres indesejados
+ex: [1,3,4]
+'''
 def get_value(line):
     clean_line = "AG[]\n"
     for i in range(0, len(clean_line)):
@@ -6,6 +9,10 @@ def get_value(line):
     line = line.split(',')
     return line
 
+'''
+Entrada: Lista com itens de coordenadas ex: [[0,0,0],[1,2,3]]
+Saida: Lista com todas as coordenada do index n
+'''
 def get_coordenada(values, n):
     v = []
     for value in values:
